9741d440f98edd7c190baf83613435a66162d9af
Jeremías <jeremias.j.riv@gmail.com>
2025-07-21 14:28:31 -0300

    User y autenticacion completo

--- backend/user-service/src/main/java/com/enterprisemanager/config/security/JwtAuthenticationFilter.java
+++ backend/user-service/src/main/java/com/enterprisemanager/config/security/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package com.enterprisemanager.config.security;
 
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.enterprisemanager.repository.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -10,6 +9,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import java.io.IOException;
 
