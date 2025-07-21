cf5f873f38ad70f6c24b55c10921954f6ccf4aaf
Jeremías <jeremias.j.riv@gmail.com>
2025-07-21 14:53:36 -0300

    Creado el customer-service

---
+++ backend/customer-service/src/main/java/com/enterprisemanager/CostumerServiceApplication.java
@@ -0,0 +1,11 @@
+package com.enterprisemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostumerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CostumerServiceApplication.class, args);
+    }
+}
